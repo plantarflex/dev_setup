d8aa0b555092882a655f447594f93096070c4d1d
plantarflex <plantarflex@naver.com>
2021-09-01 00:56:05 +0900

    add flake8 settings

--- darwin/vscode_setup.sh
+++ darwin/vscode_setup.sh
@@ -1,3 +1,4 @@
 cp settings_vscode.json Library/Application\ Support/Code/User/settings.json
 #TODO: node version maintenance...
+pip3 install flake8
 npm install -g eslint@4.x babel-eslint@8 eslint-plugin-react
