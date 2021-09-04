895e99e2477882841d035b22ea87dad19c6c5d41
plantarflex <plantarflex@naver.com>
2021-09-04 23:26:01 +0900

    specify node and eslint path to settings.json

--- darwin/vscode_setup.sh
+++ darwin/vscode_setup.sh
@@ -1,3 +1,4 @@
 cp settings_vscode.json ~/Library/Application\ Support/Code/User/settings.json
+python3 -m pip install -U autopep8
 python3 -m pip install -U flake8
 sudo npm install -g eslint@4.x babel-eslint@8 eslint-plugin-react
