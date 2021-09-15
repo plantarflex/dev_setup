e5f86464fedebe300a36ba3174adcb142ecfe31b
Minkyu Song/송민규 <31844264+plantarflex@users.noreply.github.com>
2021-09-15 22:12:00 +0900

    Update essentials.sh

--- darwin/essentials.sh
+++ darwin/essentials.sh
@@ -1,5 +1,6 @@
 # homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.bashrc
 
 # python to /usr/local/bin (check out bashrc)
 brew install python3.9
